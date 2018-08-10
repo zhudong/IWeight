9d277e99d41e528a26f76bdaf60bd3d78d47e836
l424533553 <17727878236@163.com>
2018-08-10 17:02:56 +0800

    提交测试 文件test.java

---
+++ app/src/main/java/com/axecom/test/TEST.java
@@ -0,0 +1,17 @@
+package com.axecom.test;
+
+import android.util.Log;
+
+/**
+ * 说明：
+ * 作者：User_luo on 2018/8/10 16:50
+ * 邮箱：424533553@qq.com
+ */
+public class TEST {
+
+    private  void test(){
+
+        Log.i("1111","421321");
+
+    }
+}
